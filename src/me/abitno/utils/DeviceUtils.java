977b6d7aed0512fbe7aada906d3fcf31940188da
Cedric Fung <root@vec.io>
2012-10-30 10:49:06 +0800

    getRawWidth in get screen width
    Remove VPlayer specific loading resources

--- src/me/abitno/utils/DeviceUtils.java
+++ src/me/abitno/utils/DeviceUtils.java
@@ -1,18 +1,38 @@
 package me.abitno.utils;
 
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.util.DisplayMetrics;
+import android.view.Display;
 
-@SuppressWarnings("deprecation")
 public class DeviceUtils {
+	@SuppressWarnings("deprecation")
 	public static int getScreenWidth(Activity ctx) {
-		return ctx.getWindowManager().getDefaultDisplay().getWidth();
+		int width;
+		Display display = ctx.getWindowManager().getDefaultDisplay();
+		try {
+			Method mGetRawW = Display.class.getMethod("getRawWidth");
+			width = (Integer) mGetRawW.invoke(display);
+		} catch (Exception e) {
+			width = display.getWidth();
+		}
+		return width;
 	}
 
+	@SuppressWarnings("deprecation")
 	public static int getScreenHeight(Activity ctx) {
-		return ctx.getWindowManager().getDefaultDisplay().getHeight();
+		int height;
+		Display display = ctx.getWindowManager().getDefaultDisplay();
+		try {
+			Method mGetRawH = Display.class.getMethod("getRawHeight");
+			height = (Integer) mGetRawH.invoke(display);
+		} catch (Exception e) {
+			height = display.getHeight();
+		}
+		return height;
 	}
-	
+
 	public static double getScreenPhysicalSize(Activity ctx) {
 		DisplayMetrics dm = new DisplayMetrics();
 		ctx.getWindowManager().getDefaultDisplay().getMetrics(dm);
