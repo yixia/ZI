79da4a4f5aa922486e15cfa946e8ff79fd80ca4b
Cedric Fung <cedric@vec.io>
2013-04-27 17:57:46 +0800

    Add helper to get a proper filename

--- src/com/yixia/zi/utils/FileUtils.java
+++ src/com/yixia/zi/utils/FileUtils.java
@@ -21,6 +21,32 @@ import java.io.IOException;
 import android.net.Uri;
 
 public class FileUtils {
+	private static final String FILE_NAME_RESERVED = "|\\?*<\":>+[]/'";
+
+	public static String getUniqueFileName(String name, String id) {
+		StringBuilder sb = new StringBuilder();
+		for (Character c : name.toCharArray()) {
+			if (FILE_NAME_RESERVED.indexOf(c) == -1) {
+				sb.append(c);
+			}
+		}
+		name = sb.toString();
+		if (name.length() > 16) {
+			name = name.substring(0, 16);
+		}
+		id = Crypto.md5(id);
+		name += id;
+		try {
+			File f = File.createTempFile(name, null);
+			if (f.exists()) {
+				f.delete();
+				return name;
+			}
+		} catch (IOException e) {
+		}
+		return id;
+	}
+
 	public static String getCanonical(File f) {
 		if (f == null)
 			return null;
